243afacc17d62729a3fcd4b3848c930223bbf14c
netwarp <resethread@gmail.com>
2020-08-31 22:08:40 +0200

    flash workds

--- controllers/FrontController.js
+++ controllers/FrontController.js
@@ -1,3 +1,124 @@
+const env = require('../env.json')
+const mailgun = require('mailgun-js')
+const domain = env.mail.domain
+const apiKey = env.mail.apiKey
+const crypto = require('crypto')
+const fs = require('fs').promises
+const nunjucks = require('nunjucks')
+const User = require('../models/User')
+
+const { body, validationResult } = require('express-validator');
+
 exports.index = async (request, response) => {
-    response.json('hello from controllers')
+    const head_title = 'Board'
+
+    await response.render('front/index.html', {
+        head_title,
+        success: request.flash('success'),
+        errors: request.flash('errors'),
+    })
 }
+
+exports.image = async (request, response) => {
+
+}
+
+exports.getRegister = async (request, response) => {
+    const head_title = 'Register'
+
+    await response.render('front/register.html', {
+        head_title
+    })
+}
+
+exports.postRegister = async (request, response) => {
+
+    const name = request.body.name
+    const email = request.body.email
+    const password = request.body.password
+    const errors = validationResult(request );
+
+    if (!errors.isEmpty()) {
+        return response.status(400).json({ errors: errors.array() });
+    }
+
+    const mg = mailgun({
+        apiKey,
+        domain
+    })
+
+    const user = await User.create({
+        name,
+        email,
+        password,
+        verify_token: token,
+        status: 'user'
+    })
+    let token = user.id
+    token += '-'
+    token +=  crypto.randomBytes(16).toString('hex')
+    let html = await fs.readFile("views/mails/register.html", "utf8");
+
+    html = nunjucks.renderString(html, {
+        token,
+        domain: env.app.domain
+    })
+
+    const data = {
+        from: 'toto1111111@yopmail.com',
+        to: email,
+        subject: 'Welcome, Please confirm your email',
+        text: 'Testing some Mailgun awesomeness!',
+        html,
+    }
+
+
+
+    await mg.messages().send(data, function (error, body) {
+        console.log(body)
+    })
+
+    await request.flash('success', 'Well done, look into your email to access your dashboard');
+
+    await response.redirect('/')
+}
+
+exports.getContact = async (request, response) => {
+
+}
+
+exports.rules = async (request, response) => {
+
+}
+
+exports.support = async (request, response) => {
+
+}
+
+exports.error = async (request, response) => {
+
+}
+
+exports.getSearch = async (request, response) => {
+
+}
+
+
+
+exports.test = async (request, response) => {
+    /*
+    request.flash('success', 'you are the boss, boss');
+    response.redirect('/')
+
+     */
+    const user = await User.create({
+        name: 'bob',
+        email: 'bob@bob2.com',
+        password: 'rezrze',
+        status: 'user',
+    })
+    console.log(user)
+
+}
+
+'toto1111111@yopmail.com'
--- index.js
+++ index.js
@@ -4,17 +4,43 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = env.app.port
 const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const flash = require('connect-flash');
 const router = require('./routes/web')
+const nunjucks = require('nunjucks')
+
+app.use(cookieParser('flash message made me crazy'))
+
+app.use(session({
+    secret: 'flash message made me crazy',
+    saveUninitialized: true,
+    resave: true,
+}))
+
+app.use(flash())
+
+app.use(function(req, res, next){
+    res.locals.success_messages = req.flash('success_messages');
+    res.locals.error_messages = req.flash('error_messages');
+    next();
+});
+
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app,
+    watch: true
+})
 
 app.use(express.static('public'))
 
 app.use(bodyParser.urlencoded({
-  extended: false
+    extended: false
 }))
 
 app.use(bodyParser.json())
-//app.use(cookieParser('toto'))
 
 app.use('/', router)
 
-app.listen(port)
+app.listen(port, () => {
+    console.log('Server starting')
+})
