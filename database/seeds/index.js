83232fd72450b06d848527907d70ef69cd5de51c
netwarp <resethread@gmail.com>
2020-09-04 19:37:28 +0200

    update seeder

--- database/seeds/index.js
+++ database/seeds/index.js
@@ -2,15 +2,16 @@ const env = require('../../env.json')
 const { Sequelize } = require('sequelize')
 const faker = require('faker')
 const bcrypt = require('bcrypt')
+const crypto = require('crypto')
 
 const  User = require('../../models/User')
+const  Board = require('../../models/Board')
+const  Thread = require('../../models/Thread')
 
 const sequelize = new Sequelize(env.db.database, env.db.username, env.db.password, {
     host: 'localhost',
     dialect: 'postgres'
 })
-
-
 
 async function ping() {
     try {
@@ -46,9 +47,53 @@ async function CollectionUsersSeeder() {
     console.log('users created')
 }
 
+async function CollectionBoardsSeeder() {
+    const user_id = 25
+    for (let i = 0; i < 300; i++) {
+        const title = faker.lorem.words(9)
+
+        const data = {
+            title,
+            slug: faker.helpers.slugify(title),
+            description: faker.lorem.words(9),
+            folder: crypto.randomBytes(12).toString('hex'),
+            image_path: 'storage/app/boards/test',
+            user_id
+        }
+
+        Board.create(data)
+    }
+}
+
+async function CollectionThreadsSeeder () {
+    const user_id = 25
+    const boards = await Board.findAll()
+    for (board of boards) {
+
+        for (let i = 0; i < 30; i++) {
+            const id = board.id
+            const title = faker.lorem.words(9) + `- ${i}`
+
+            const data = {
+                board_id: id,
+                title,
+                slug: faker.helpers.slugify(title),
+                content: faker.lorem.words(9),
+                folder: '11111',
+                image: '',
+                image_path: '',
+                user_id,
+            }
+            Thread.create(data)
+        }
+    }
+}
+
 async function main() {
-    await ping()
-    await CollectionUsersSeeder()
+    //await ping()
+    //await CollectionUsersSeeder()
+    //await CollectionBoardsSeeder()
+    await CollectionThreadsSeeder()
 }
 main()
 
